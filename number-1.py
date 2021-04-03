598d009192b988981cbc6397e774060e3c93ebb8
khalifa2348 <76643675+khalifa2348@users.noreply.github.com>
2021-04-03 12:29:46 +0100

    Add files via upload

---
+++ number-1.py
@@ -0,0 +1,37 @@
+print('Question 1')
+MyName='SHEHU KHALIFA MUHAMMAD'
+
+MyDepartment='COMPUTER SCIENCE DEPARTMENT'
+
+print('My name is',MyName)
+
+print('I am from',MyDepartment)
+
+print('Question 2')
+scare='boo!'
+
+print(scare)
+
+print('Question 3')
+numb1=5
+numb2=7
+numb3=numb1 + numb2
+print(numb1,numb2,numb3)
+print('Quetion 4')
+result=12/0.5
+print(result)
+print('Question 5')
+
+Muhammad=15 + 20
+print(Muhammad)
+print('Question 6')
+Khalifa=8%3
+print(Khalifa)
+print('Question 7')
+result1=2 * 4
+
+result2=4 + 2
+
+result3=result1 * result2
+
+print(result3)
